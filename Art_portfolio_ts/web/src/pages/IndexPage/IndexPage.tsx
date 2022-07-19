11a560c5db42e527cf447f0b2406e76d3f2e107a
laycookie <lodennis6@gmail.com>
2022-07-18 18:09:17 -0700

    Button V1
    Added Button on Home page still a bit buggy

--- Art_portfolio_ts/web/src/pages/IndexPage/IndexPage.tsx
+++ Art_portfolio_ts/web/src/pages/IndexPage/IndexPage.tsx
@@ -5,9 +5,9 @@ import Navbar from '../../components/Navbar'
 import Login from '../../components/Login'
 
 const IndexPage = () => {
-  const [mainTitleDisplay, setmainTitleDisplay] = useState("50")
-  const [mainTitleBorder, setmainTitleBorder] = useState(100)
-  console.log(mainTitleDisplay)
+  //Title of the page
+  const [mainTitleDisplay, setmainTitleDisplay] = useState("0")
+  const [mainTitleBorder, setmainTitleBorder] = useState(0)
 
   function showHeader() {
     setmainTitleBorder(100)
@@ -16,7 +16,23 @@ const IndexPage = () => {
   function hideHeader() {
     setmainTitleBorder(0)
     setmainTitleDisplay("0")
-  }//style={{borderWidth: "0 0 8px"}}
+  }
+  //End of title of the page
+
+  //Button animation
+  const [arrowOffSet, setArrowOffSet] = useState(0)
+  const [arrowLength, setArrowLength] = useState(0)
+
+  function onHoverSetArrowLength() {
+    setArrowOffSet(-3)
+    setArrowLength(96)
+  }
+  function offHoverSetArrowLength() {
+    setArrowOffSet(0)
+    setArrowLength(0)
+  }
+
+  //End of button animation
   
   return (
     <>
@@ -32,19 +48,35 @@ const IndexPage = () => {
         </nav>
       </header>
       <div className="transition-all flex justify-center" id='TitleText'>
-        <div onMouseEnter={showHeader} onMouseLeave={hideHeader}> 
+        <div onMouseEnter={showHeader} onMouseLeave={hideHeader}>
           <h1 className='text-red-500 h1Font pb-1' >Art Portfolio</h1>
-          <div className='bg-red-500 flex justify-center transition-all' style={{height: "8px", width:`${mainTitleBorder}%`, margin:"auto"}}></div>
-          <h3 className='text-red-500 flex justify-center pt-2 transition-all' id='TitleSecondaryText' style={{opacity: mainTitleDisplay}}>By Dennis Lonoshchuk</h3>
+          <div className='bg-red-500 flex justify-center transition-all rounded-full' style={{height: "8px", width:`${mainTitleBorder}%`, margin:"auto"}}></div>
+          <h3 className='text-red-500 flex justify-center pt-2 transition-all font-bold text-xl' id='TitleSecondaryText' style={{opacity: mainTitleDisplay}}>By Dennis Lonoshchuk</h3>
         </div>
       </div>
       
-      <div>
-        <button>
-          <svg></svg>
-          <svg></svg>
+      {/* button */}
+      <div className="flex justify-center mt-24 transition-all">
+        <button className='absolute transition-all' style={{transform: `translate(0, ${arrowOffSet}rem)`}}>
+
+          <div className='flex justify-center m-auto'>
+            <div style={{height:`${arrowLength + 24}px`}} className="w-28 absolute -translate-y-2" onMouseEnter={onHoverSetArrowLength} onMouseLeave={offHoverSetArrowLength}/> {/* this div covers the arrow */}
+            <div style={{height:`${arrowLength}px`}} className="transition-all relative">
+              <div style={{width:"7px",}} className='bg-red-500 h-full rounded-full transition-all ' onMouseEnter={onHoverSetArrowLength} onMouseLeave={offHoverSetArrowLength}/>
+              <path fill-rule="evenodd" clip-rule="evenodd" d='M2 2' fill="#ef4444"/>
+            </div>
+          </div>
+          <div onMouseEnter={onHoverSetArrowLength} onMouseLeave={offHoverSetArrowLength} className="relative -translate-y-12">
+            <svg width="90" height="59" viewBox="0 0 107 59" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path fill-rule="evenodd" clip-rule="evenodd" d="M1.83438 1.80174C3.60073 0.0534198 6.44993 0.0680311 8.19826 1.83438L53.5 47.6031L98.8017 1.83438C100.55 0.0680311 103.399 0.0534198 105.166 1.80174C106.932 3.55007 106.947 6.39927 105.198 8.16562L57.409 56.4476C55.2566 58.6221 51.7434 58.6221 49.591 56.4476L1.80174 8.16562C0.0534198 6.39927 0.0680311 3.55007 1.83438 1.80174Z" fill="#ef4444"/>
+            </svg>
+          </div>
+
         </button>
-      </div>
+
+        </div>
+      {/* button end */}
+
     </>
   )
 }
